99f8cc109f127488b96fb10a73070f931516eb2e
CheliJoaquin <163900709+CheliJoaquin@users.noreply.github.com>
2024-04-22 13:40:56 -0300

    Add files via upload

---
+++ Tp 3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	float n1,n2,n3;
+	cout<<"Ingrese 3 numeros:"<<endl;
+	cin>>n1;
+	cin>>n2;
+	cin>>n3;
+	cout<<"El promedio es:  "<<(n1+n2+n3)/3.0<<endl;
+  
+}
