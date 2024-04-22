6ea030ac5d669813a31d10467a123b3723a4d91f
CheliJoaquin <163900709+CheliJoaquin@users.noreply.github.com>
2024-04-22 13:13:29 -0300

    Add files via upload

---
+++ TP 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n1,n2,m;
+
+cout<<"Ingrese la base"<<endl;
+cin>>n1;
+cout<<"Ingrese la altura"<<endl;
+cin>>n2;
+
+m=n1*n2;
+
+cout<<"El resultado de la multiplicacion es:"<<m<<endl;
+
+return 0;
+}
