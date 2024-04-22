5b1397c889140122af104edec30ed19aa1e84fc0
CheliJoaquin <163900709+CheliJoaquin@users.noreply.github.com>
2024-04-22 13:59:45 -0300

    Update Tp 3.cpp

--- Tp 3.cpp
+++ Tp 3.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 int main ()
 {
-	float n1,n2,n3;
+	float n1,n2,n3,d;
 	cout<<"Ingrese 3 numeros:"<<endl;
 	cin>>n1;
 	cin>>n2;
 	cin>>n3;
-	cout<<"El promedio es:  "<<(n1+n2+n3)/3.0<<endl;
+	d=3;
+	cout<<"El promedio es:  "<<(n1+n2+n3)/d<<endl;
   
 }
